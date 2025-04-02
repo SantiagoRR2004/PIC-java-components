9340afcb671f5b142221bb6ca26d5aa41da41332
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-04-02 21:33:49 +0200

    Fixes #25; Enhance DeviceDataManager to integrate MqttClientConnector for improved MQTT connection management; update tests for connection, disconnection, and publish/subscribe functionality

--- src/test/java/programmingtheiot/part03/integration/connection/MqttClientControlPacketTest.java
+++ src/test/java/programmingtheiot/part03/integration/connection/MqttClientControlPacketTest.java
@@ -40,12 +40,15 @@ public class MqttClientControlPacketTest {
 	// member var's
 
 	private MqttClientConnector mqttClient = null;
+	private int delay;
 
 	// test setup methods
 
 	@Before
 	public void setUp() throws Exception {
 		this.mqttClient = new MqttClientConnector();
+		this.delay = ConfigUtil.getInstance().getInteger(ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.KEEP_ALIVE_KEY,
+			ConfigConst.DEFAULT_KEEP_ALIVE);
 	}
 
 	@After
@@ -56,19 +59,58 @@ public class MqttClientControlPacketTest {
 
 	@Test
 	public void testConnectAndDisconnect() {
-		// TODO: implement this test
+		assertTrue(this.mqttClient.connectClient());
+		assertFalse(this.mqttClient.connectClient());
+
+		try {
+			Thread.sleep(delay * 1000 + 5000);
+		} catch (Exception e) {
+			// ignore
+		}
+
+		assertTrue(this.mqttClient.disconnectClient());
+		assertFalse(this.mqttClient.disconnectClient());
+		_Logger.info("testConnectAndDisconnect() is complete.");
 	}
 
 	@Test
 	public void testServerPing() {
-		// TODO: implement this test
+		assertTrue(this.mqttClient.connectClient());
+		assertTrue(this.mqttClient.isConnected());
+
+		try {
+			Thread.sleep(delay * 1000 + 5000);
+		} catch (Exception e) {
+			// ignore
+		}
+
+		assertTrue(this.mqttClient.isConnected());
+		assertTrue(this.mqttClient.disconnectClient());
+		_Logger.info("testServerPing() is complete.");
 	}
 
 	@Test
 	public void testPubSub() {
-		// TODO: implement this test
-		//
 		// IMPORTANT: be sure to use QoS 1 and 2 to see ALL control packets
+		int[] qosLevels = { 0, 1, 2 };
+
+		assertTrue(this.mqttClient.connectClient());
+
+		for (int qos : qosLevels) {
+			assertTrue(this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_MGMT_STATUS_MSG_RESOURCE, qos));
+
+			try {
+				Thread.sleep(delay * 1000);
+			} catch (Exception e) {
+				// ignore
+			}
+			assertTrue(this.mqttClient.publishMessage(ResourceNameEnum.CDA_MGMT_STATUS_MSG_RESOURCE,  "Test message: QoS " + qos, qos));
+	
+			assertTrue(this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_MGMT_STATUS_MSG_RESOURCE));
+		}
+
+		assertTrue(this.mqttClient.disconnectClient());
+		_Logger.info("testPubSub() is complete.");
 	}
 
 }
